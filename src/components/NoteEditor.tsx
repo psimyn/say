755666b170c5f0baa0ccd67f02994a2c304b8a8c
Addy Osmani <addyosmani@gmail.com>
2024-12-10 15:00:29 -0800

    [ux][editor] disable checklist plugin
    This was causing a premium plugin error

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -263,12 +263,11 @@ export const NoteEditor: React.FC<NoteEditorProps> = ({
           plugins: [
             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
             'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
-            'checklist'
+            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
           ],
           toolbar: 'undo redo | blocks | ' +
             'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist checklist outdent indent | ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
             'removeformat | help',
           content_style: 'body { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif; font-size: 14px }',
           skin: 'oxide',
